fe84f764225fb2c140d4e87482ce5c77e80192ef
leonnorth <leonnorth@gmail.com>
2015-07-21 12:59:48 +1200

    add tech skills

--- js/life.js
+++ js/life.js
@@ -1,23 +1,41 @@
-
 var width,
 	height,
 	svgContainer,
 	arrayWidth,
 	arrayHeight,
 	squareSize,
+	speed,
 	toggle,
 	remainderXBuffer,
 	pause,
 	maxShade,
 	remainderYBuffer;
+
+
 var arrayA;
 var	arrayB;
 
+var colors;
+/* COLOUR SCHEME
+Green : (88, 140, 126,), (#588C7E)
+Yellow: (242, 227, 148), (#F2E394);
+Orange: (242, 174, 114),(#F2AE72);
+Red:(217, 100, 89),(#D96459);
+Crimson:(140, 70, 70),(#8C4646);
+*/
+
 function init(){
+
+	//speed in ms
+	speed = 300;
+
+	//set colors
+	colors = ["#588C7E","#F2E394","#F2AE72","#D96459","#8C4646"];
 	
-	squareSize = 20;
-	width = window.innerWidth/2;
-	height = window.innerHeight/2;
+	squareSize = 8;
+	width = document.getElementById('lifewell').offsetWidth;
+	height = document.getElementById('lifewell').offsetHeight;//window.innerHeight/4;
+	// console.log(height);
 	// height = height - $("#nav").height(); // remove height of nav bar
 	pause = false;
 
@@ -29,123 +47,19 @@ function init(){
 	remainderXBuffer = ((width - (arrayWidth*squareSize))/2) + squareSize/2;
 	remainderYBuffer = ((height - (arrayHeight*squareSize))/2) + squareSize/2;
 
-	// window.addEventListener("keydown", checkKeyPressed, false);
+	// put a listener on the title well
+	// so when it's clicked, it will re-seed the life game
+	d3.select("#lifewell").on("click", function(){initArrays();})
 
 	svgContainer = d3.select(".life")
 		.append("svg")
 		.attr("width", width)
 		.attr("height", height)
-	// 	.on("click", function(d){
-	// 		var mouseX = d3.mouse(this)[0],
-	// 	mouseY = d3.mouse(this)[1];
-	// 		addNewCell(mouseX, mouseY);
-	// })
-  	
-	// console.log("width:"+width+" height:"+height+" arrayWidth:"+arrayWidth+" arrayHeight:"+arrayHeight+" arrayWidth*squareSize:"+(arrayWidth*squareSize)+" arrayHeight*squareSize:"+(arrayHeight*squareSize)+" remainderYBuffer:"+remainderYBuffer+" remainderXBuffer:"+remainderXBuffer);
 
 	draw();
 	
 }
 
-// function reset(){
-// 	pause = true;
-// 	//reset arrayA
-// 	arrayA = new Array(arrayWidth);
-// 	arrayB = new Array(arrayWidth);
-// 	for (var i = 0; i < arrayWidth; i++){
-// 		arrayA[i] = new Array(arrayHeight);
-// 		arrayB[i] = new Array(arrayHeight);
-// 		for (var j = 0; j < arrayHeight; j++){
-// 			arrayA[i][j] = {"alive":false, "age":0};
-// 			arrayB[i][j] = {"alive":false, "age":0};
-// 		} 
-// 	}
-
-	// seed arrayA
-// 	arrayA[1][1].alive = arrayA[2][2].alive = arrayA[3][3].alive = arrayA[4][3].alive = true;
-// 	arrayA[1][2].alive = arrayA[2][1].alive = arrayA[3][4].alive = arrayA[4][4].alive = true;
-// 	arrayA[1][1].age = arrayA[2][2].age = arrayA[3][3].age = arrayA[4][3].age = 1;
-// 	arrayA[1][2].age = arrayA[2][1].age = arrayA[3][4].age = arrayA[4][4].age = 1;
-
-// 	arrayA[0][10].alive = arrayA[1][9].alive = arrayA[2][9].alive = arrayA[3][9].alive = true;
-// 	arrayA[4][9].alive = arrayA[4][10].alive = arrayA[4][11].alive = arrayA[3][12].alive = true;
-// 	arrayA[0][12].alive = true;
-// 	arrayA[0][10].age = arrayA[1][9].age = arrayA[2][9].age = arrayA[3][9].age = 1;
-// 	arrayA[4][9].age = arrayA[4][10].age = arrayA[4][11].age = arrayA[3][12].age = 1;
-// 	arrayA[0][12].age = 1;
-
-// 	pause = false;
-// }
-
-// function writeInstructions(){
-// 	console.log("writeInstructions")
-// 	var instructions = "hi, SPACE to pause R to reset"
-
-// 	svgContainer.append("text")
-// 		.text(instructions)
-// 		.attr("x", 0)
-// 		.attr("y", 0)
-// 		.attr("dy", 0)
-// 		.attr("text-anchor", "middle")
-// 		.attr("fill", "white")
-// 		.attr("font-size", "50px")
-// 		.attr("font-family", "sans-serif")
-// 		.attr("transform", function(d, i){return"translate("+width/2+","+height/2+")";})
-// 		.call(wrap, Math.max(width/2, 300))
-
-	/*
-Wraps text in the width given.
-*/
-// function wrap(text, wid) {
-// 	text.each(function() {
-// 		var text = d3.select(this),
-// 			words = text.text().split(/\s+/).reverse(),
-// 			word,
-// 			line = [],
-// 			lineNumber = 0,
-// 	     	lineHeight = 1.1, // ems
-// 	        y = text.attr("y"),
-// 	        dy = parseFloat(text.attr("dy")),
-// 	        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-//         while (word = words.pop()) {
-//         	line.push(word);
-//         	tspan.text(line.join(" "));
-//         	if (tspan.node().getComputedTextLength() > wid) {
-//         		line.pop();
-//         		tspan.text(line.join(" "));
-//         		line = [word];
-//         		tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-//         	}
-//         }
-//     });
-// }
-
-// }
-
-// function checkKeyPressed(e) {
-
-// 	//r to reset
-// if (e.keyCode == "82") {
-//       reset();
-//   }
-// 	//spacebar to pause
-//   if (e.keyCode == "32") {
-//       pause = !pause;
-//   }
-// }
-
-// function addNewCell(mouseX, mouseY){
-
-// cellX = parseInt((mouseX - remainderXBuffer + (squareSize/2))/squareSize);
-// cellY = parseInt((mouseY - remainderYBuffer + (squareSize/2))/squareSize);
-
-// arrayA[cellX][cellY].alive = true;
-// arrayA[cellX][cellY].age = Math.max(arrayA[cellX][cellY].age, 1);
-
-// //find the correct cell by the xy coordinates and update it's size
-// var id = '#id'+cellY.toString()+'X'+(cellX).toString();
-// svgContainer.select(id).transition().duration(100).attr("r", squareSize/2);
-// }
 
 function initArrays(){
 
@@ -160,34 +74,21 @@ function initArrays(){
 		arrayA[i] = new Array(arrayHeight);
 		arrayB[i] = new Array(arrayHeight);
 		for (var j = 0; j < arrayHeight; j++){
-			arrayA[i][j] = {"alive":false, "age":0};
-			arrayB[i][j] = {"alive":false, "age":0};
+
+			//decide aliveness
+			var aliveness = false;
+			if (Math.random() > .7){
+				aliveness = true;
+			}
+			arrayA[i][j] = {"alive":aliveness, "age":0};
+			arrayB[i][j] = {"alive":aliveness, "age":0};
 		} 
 	}
-
-	// seed arrayA
-	// arrayA[1][1].alive = arrayA[2][2].alive = arrayA[3][3].alive = arrayA[4][3].alive = true;
-	// arrayA[1][2].alive = arrayA[2][1].alive = arrayA[3][4].alive = arrayA[4][4].alive = true;
-	// arrayA[1][1].age = arrayA[2][2].age = arrayA[3][3].age = arrayA[4][3].age = 1;
-	// arrayA[1][2].age = arrayA[2][1].age = arrayA[3][4].age = arrayA[4][4].age = 1;
-	
-	arrayA[0][10].alive = arrayA[1][9].alive = arrayA[2][9].alive = arrayA[3][9].alive = true;
-	arrayA[4][9].alive = arrayA[4][10].alive = arrayA[4][11].alive = arrayA[3][12].alive = true;
-	arrayA[0][12].alive = true;
-	arrayA[0][10].age = arrayA[1][9].age = arrayA[2][9].age = arrayA[3][9].age = 1;
-	arrayA[4][9].age = arrayA[4][10].age = arrayA[4][11].age = arrayA[3][12].age = 1;
-	arrayA[0][12].age = 1;
 }
 
 
 
 function draw(){
-	// pink background
-	// svgContainer.append("rect")
-	// 	.attr("width", "100%")
-	// 	.attr("height", "100%")
-	// 	.attr("fill", "pink");
-	// writeInstructions();
   	drawInit();
 	run();
 }
@@ -210,19 +111,22 @@ function drawInit(){
     .data( function(d, i, j){ return d;})
     .enter()
     .append("circle")
-    .on('mouseover', tip.show)
-    .on('mouseout', function() {
-      d3.select(".d3-tip")
-      .transition()
-        .delay(1700)
-        .duration(900)
-        .style("opacity",0)
-        .style('pointer-events', 'none')
-      })
+    //.on('mouseover', tip.show)
+    //.on('mouseout', function() {
+      // d3.select(".d3-tip")
+      // .transition()
+      //   .delay(1700)
+      //   .duration(900)
+      //   .style("opacity",0)
+      //   .style('pointer-events', 'none')
+      // })
     .attr("id", function(d, i, j){return "id"+i.toString()+"X"+j.toString();})
-    .attr("fill", function(d){return "rgb(255,255,255)";})
+    //.attr("fill", function(d){return "rgb(255,255,255)";})
     .attr("cx", function(d, i, j){ return j * squareSize + remainderXBuffer ;})
     .attr("cy", function(d, i, j){ return i * squareSize + remainderYBuffer ;})
+    .attr("fill", function(){
+    	var colo = parseInt(Math.random()*colors.length); 
+    	return colors[colo];})
     .style("opacity", 0.7)
     .attr("r", function(d){
 				if (d.alive){ return (squareSize/2);} else {return 0;} 
@@ -230,18 +134,18 @@ function drawInit(){
 
 }
 
-function run(){
-	var running = setInterval(function(){
-		if (!pause){
-		
+
+var start = null;
+function run(timestamp){
+	if (!start) start = timestamp;
+
+	var progress = timestamp - start;
+	if (progress >= speed){
 		tick();
 		drawTick();
+		start = timestamp;
 	}
-    },500);
-    function done(){
-    	console.log("done");
-      clearInterval(running);      
-    } 
+	window.requestAnimationFrame(run);
 }
 
 function tick(){
@@ -251,59 +155,46 @@ function tick(){
 		for (row = 0; row < arrayHeight; row++){
 			var aliveNeighbours = checkNeighbours(col, row);
 
-	// Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-		if(arrayA[col][row].alive && aliveNeighbours < 2){
+		// Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+			if(arrayA[col][row].alive && aliveNeighbours < 2){
+				arrayB[col][row].alive = false;
+				arrayB[col][row].age = 0;
+			}
+		// Any live cell with two or three live neighbours lives on to the next generation.
+			else if(arrayA[col][row].alive && aliveNeighbours < 4){
+				arrayB[col][row].alive = true;
+				arrayB[col][row].age = arrayA[col][row].age+1;
+			}
+		// Any live cell with more than three live neighbours dies, as if by overcrowding.
+			else if(arrayA[col][row].alive){
+				arrayB[col][row].alive = false;
+				arrayB[col][row].age = 0;
+			}
+		// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+			else if(!arrayA[col][row].alive && aliveNeighbours === 3){
+				arrayB[col][row].alive = true;
+				arrayB[col][row].age = 1;
+			}
+		}
+	}
+
+		//set arrayA and clear arrayB
+	for (col = 0; col < arrayWidth; col++){
+		for (row = 0; row < arrayHeight; row++){
+			arrayA[col][row].alive = arrayB[col][row].alive;
+			arrayA[col][row].age = arrayB[col][row].age;
 			arrayB[col][row].alive = false;
 			arrayB[col][row].age = 0;
 		}
-	// Any live cell with two or three live neighbours lives on to the next generation.
-		else if(arrayA[col][row].alive && aliveNeighbours < 4){
-			arrayB[col][row].alive = true;
-			arrayB[col][row].age = arrayA[col][row].age+1;
-		}
-	// Any live cell with more than three live neighbours dies, as if by overcrowding.
-		else if(arrayA[col][row].alive){
-			arrayB[col][row].alive = false;
-			arrayB[col][row].age = 0;
-		}
-	// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-		else if(!arrayA[col][row].alive && aliveNeighbours === 3){
-			arrayB[col][row].alive = true;
-			arrayB[col][row].age = 1;
-		}
-		}
 	}
-
-	//set arrayA and clear arrayB
-for (col = 0; col < arrayWidth; col++){
-	for (row = 0; row < arrayHeight; row++){
-		arrayA[col][row].alive = arrayB[col][row].alive;
-		arrayA[col][row].age = arrayB[col][row].age;
-		arrayB[col][row].alive = false;
-		arrayB[col][row].age = 0;
-	}
-}
-
-}
-
-function printArray(){
-	console.log("new print");
-	var strLine = " ";
-for (col = 0; col < arrayWidth; col++){
-	for (row = 0; row < arrayHeight; row++){
-		strLine = strLine + " " + (arrayA[col][row].alive ? "T" : "F" );
-	}
-	console.log("");
-	console.log(strLine);
-	strLine = " "
-}
 }
 
 //count number of surrounding neighbours
 function checkNeighbours(col, row){
-//function for using modulo and keeping output positive
-  Number.prototype.mod = function(n) { return ((this%n)+n)%n; }
-  var neighbours = 0;
+	//function for using modulo and keeping output positive
+	Number.prototype.mod = function(n) { return ((this%n)+n)%n; }
+	
+	var neighbours = 0;
 
    if (arrayA[(col-1).mod(arrayWidth)][(row-1).mod(arrayHeight)].alive) {neighbours++;} 
    if (arrayA[col][(row-1).mod(arrayHeight)].alive) {neighbours++;} 
@@ -327,13 +218,10 @@ function drawTick(){
       .selectAll("circle")
       .data( function(d, i, j){ return d;})
     	.transition()
-    	.duration(500)
-    	.attr("fill", function(d){var shade =255 - ((d.age/10) * 254);return"rgb("+shade+","+shade+","+shade+")"})
+    	.duration(speed)
     	.attr("r", function(d){
 				if (d.alive){ return (squareSize/2);} else {return 0;} 
 			;})
 }
 
 init();
-
-
